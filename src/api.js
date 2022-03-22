6695c74236d813588f1a0589072eeefe5065475a
viacheslav_demchenko <frontend-ninja@yandex.ru>
2022-03-22 17:45:44 +0300

    corrected json files links

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 const getProducts = async () => {
-    const response = await fetch('/products.json');
+    const response = await fetch('/react-sushi-spa/products.json');
     return await response.json();
 }
 
 const getAllCategories = async () => {
-    const response = await fetch('/categories.json');
+    const response = await fetch('/react-sushi-spa/categories.json');
     return await response.json();
 }
 
